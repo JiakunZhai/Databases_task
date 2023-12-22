5f9c4321a01494bb936599f0087943da1f5db36d
ZhaiJiakun <j2643992674@outlook.com>
2023-12-22 18:13:41 +0800

    添加主程序和user类

---
+++ main.py
@@ -0,0 +1,18 @@
+import test_kehu
+
+def main():
+    # 客户进行系统的时候提示登录
+    print("===========欢迎使用点单系统===========\n")
+    print("=========== version 0.1 =============\n")
+    print("1 - 用户登录\n"+"2 - 还没有账号？立刻注册\n"+"3 - 忘记账户名？立刻修改\n"+"4 - 退出\n")
+    num = input("请输入对应的操作序号：")
+
+    # while(True):
+    #     if num == 1:
+    #         user = test_kehu.USER()
+    #         user.userlogon()
+
+    # print("hello world")1
+
+if __name__ == '__main__':
+    main()
