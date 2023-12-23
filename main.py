3052ba08f05d421cd38051d9a5a50e1d8443df91
ZhaiJiakun <j2643992674@outlook.com>
2023-12-23 15:12:53 +0800

    主函数

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import test_kehu
+import user
+import menus
+import worker
+import oder
+import foodtable
+import pymysql
 
 def main():
     # 客户进行系统的时候提示登录
     print("===========欢迎使用点单系统===========\n")
     print("=========== version 0.1 =============\n")
     print("1 - 用户登录\n"+"2 - 还没有账号？立刻注册\n"+"3 - 忘记账户名？立刻修改\n"+"4 - 退出\n")
-    num = input("请输入对应的操作序号：")
+    num = input("请输入对应的操作序 号：")
 
     # while(True):
     #     if num == 1:
@@ -13,6 +18,7 @@ def main():
     #         user.userlogon()
 
     # print("hello world")1
+
 
 if __name__ == '__main__':
     main()
